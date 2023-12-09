790d5967707f07caf23470b66a75fa5496bda0cd
Ирина Мартоян <surenira@icloud.com>
2023-12-09 20:56:18 +0400

    clock fix

--- src/s2-homeworks/hw09/Clock.tsx
+++ src/s2-homeworks/hw09/Clock.tsx
@@ -32,7 +32,6 @@ function Clock() {
     }
 
     const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-    const month = ["January","February","March","April","May","June","July","August","September","October","November","December"];
 
     const addZero = (number: number) => number < 10 ? '0' + number : number
 
@@ -45,14 +44,18 @@ function Clock() {
 
     const stringDate =
         <div>
-            <span>{addZero(date.getDay())}:</span>
-            <span>{addZero(date.getMonth())}:</span>
+            <span>{addZero(date.getDay()+3)}:</span>
+            <span>{addZero(date.getMonth()+1)}:</span>
             <span>{date.getFullYear()}</span>
         </div> || <br/> // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
 
     // день недели на английском, месяц на английском (https://learn.javascript.ru/intl#intl-datetimeformat)
     const stringDay = weekday[date.getDay()] || <br/> // пишут студенты
-    const stringMonth = month[date.getMonth()] || <br/> // пишут студенты
+    // const stringMonth = month[date.getMonth()] || <br/>
+    const mont = new Intl.DateTimeFormat("en", {
+        month: "long"
+    })
+    const stringMonth = mont.format(date)  || <br/>
 
     return (
         <div className={s.clock}>
